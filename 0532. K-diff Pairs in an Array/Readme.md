208204ba004fc9dfd712a3a8a6986995983e9037
Yabadado <46954774+Yabadado@users.noreply.github.com>
2022-02-09 14:41:27 +0800

    2022/02/09

--- 0532. K-diff Pairs in an Array/Readme.md
+++ 0532. K-diff Pairs in an Array/Readme.md
@@ -8,3 +8,8 @@ Sapce Complexity : O(n)
 
 * Runtime: 52 ms, faster than 16.64% of C++ online submissions for K-diff Pairs in an Array.
 * Memory Usage: 16 MB, less than 7.05% of C++ online submissions for K-diff Pairs in an Array.
+
+Optimize
+ 
+* Runtime: 20 ms, faster than 73.03% of C++ online submissions for K-diff Pairs in an Array.
+* Memory Usage: 13.3 MB, less than 37.26% of C++ online submissions for K-diff Pairs in an Array.
--- 0532. K-diff Pairs in an Array/sol1.cpp
+++ 0532. K-diff Pairs in an Array/sol1.cpp
@@ -30,8 +30,35 @@ public:
     }
 };
 
-
-/*
-1. hash table -> int, vector(index)
-2. sort + two pointer
-*/
+/* optimize */
+class Solution {
+public:
+    int findPairs(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
+        
+        for(int i=0; i<nums.size(); i++){
+            mp[nums[i]]++;
+        }
+        sort(nums.begin(), nums.end());
+        
+        int i = 0, j = 0;
+        int ans =0;
+        
+        for(; i<nums.size();i++){
+            if(i!=0 && nums[i]==nums[i-1])
+                continue;
+            
+            int target = nums[i] + k;
+            
+            if(mp.find(target) != mp.end()){
+                
+                if(target == nums[i] && mp[target]==1 )
+                    continue;
+                
+                ans++;
+            }
+        }
+        
+        return ans;
+    }
+};
