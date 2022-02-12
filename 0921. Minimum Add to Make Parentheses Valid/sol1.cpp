d2cd4f81de4c65ff5755139acc82a3a6b1fa87e8
Yabadado <46954774+Yabadado@users.noreply.github.com>
2022-02-12 15:55:01 +0800

    2022/02/12

--- 0921. Minimum Add to Make Parentheses Valid/sol1.cpp
+++ 0921. Minimum Add to Make Parentheses Valid/sol1.cpp
@@ -3,6 +3,7 @@ public:
     int ans = 0;
     int minAddToMakeValid(string s) {
         int cnt = 0;
+        //紀錄非閉區間 -> cnt<0 表示要補'('  cnt = 待補 ')' 數量
         for(int i=0; i<s.size(); i++){
             if(s[i]=='(')
                 cnt++;
