2661f4668c313aade7e5c3dae63d79a8e7f782b3
Yabadado <46954774+Yabadado@users.noreply.github.com>
2022-02-12 15:45:54 +0800

    2022/02/12

---
+++ 0921. Minimum Add to Make Parentheses Valid/Readme.md
@@ -0,0 +1,10 @@
+## Question : https://leetcode.com/problems/minimum-add-to-make-parentheses-valid/
+
+#### sol1.cpp
+Greedy
+
+Time complexity:O(n)
+Sapce complexity:O(1)
+
+* Runtime: 0 ms, faster than 100.00% of C++ online submissions for Minimum Add to Make Parentheses Valid.
+* Memory Usage: 6.3 MB, less than 47.46% of C++ online submissions for Minimum Add to Make Parentheses Valid.
---
+++ 0921. Minimum Add to Make Parentheses Valid/sol1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int ans = 0;
+    int minAddToMakeValid(string s) {
+        int cnt = 0;
+        for(int i=0; i<s.size(); i++){
+            if(s[i]=='(')
+                cnt++;
+            else if(s[i]==')')
+                cnt--;
+            if(cnt<0){
+                ans++;
+                cnt=0;
+            }
+                
+        }
+        return ans + cnt;
+    }
+};
