66ad19d6ef0e16aa02b805d02b184145fa83f8bc
Yabadado <46954774+Yabadado@users.noreply.github.com>
2022-06-07 15:58:41 +0800

    2022/06/07

---
+++ 0088. Merge Sorted Array/Readme.md
@@ -0,0 +1,10 @@
+## Question:https://leetcode.com/problems/merge-sorted-array/
+
+#### sol1.cpp
+two pointer
+
+* time comp:O(m+n)
+* space comp:O(1)
+
+* Runtime: 3 ms, faster than 72.68% of C++ online submissions for Merge Sorted Array.
+* Memory Usage: 8.9 MB, less than 90.29% of C++ online submissions for Merge Sorted Array.
---
+++ 0088. Merge Sorted Array/sol1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int ptr1 = m-1, ptr2 = n-1, ptrAdd = nums1.size()-1;
+        
+        while(ptr1>=0 && ptr2>=0){
+            if(nums1[ptr1] > nums2[ptr2])
+                nums1[ptrAdd--] = nums1[ptr1--];
+            else
+                nums1[ptrAdd--] = nums2[ptr2--];
+        }
+        while(ptr1>=0){
+            nums1[ptrAdd--] = nums1[ptr1--];
+        }
+        while(ptr2>=0){
+            nums1[ptrAdd--] = nums2[ptr2--];
+        }
+    }
+};
